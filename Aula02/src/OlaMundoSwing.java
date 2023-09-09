feac308f164166313f74d5890a963b9b036c1a30
Cesar-Zanellato <cesar_zanellato@estudante.sesisenai.org.br>
2023-09-09 17:16:32 -0300

    Swing

--- Aula02/src/OlaMundoSwing.java
+++ Aula02/src/OlaMundoSwing.java
@@ -1,7 +1,62 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 
-public class OlaMundoSwing {
+public class OlaMundoSwing extends JFrame {
+
+    private static JLabel labelNome = new JLabel("Digite seu nome:");
+    private static JLabel outputLabel = new JLabel();
+    private static JTextField inputNome = new JTextField(25);
+    private static JButton buttonNome = new JButton("Enviar");
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        OlaMundoSwing window = new OlaMundoSwing();
+
+        // Propriedades básicas da janela;
+        window.setSize(500, 500);
+        window.setVisible(true);
+        window.setTitle("Olá Mundo Swing");
+
+        // Definindo posição;
+        window.setLayout(new FlowLayout());
+        // window.setLayout(new GridLayout(3,2)); // Linhas e Colunas;
+
+        window.getContentPane().setBackground(new Color(100, 100, 200));
+        inputNome.setForeground(new Color(0, 0, 0));
+
+        labelNome.setFont(new Font("Calibri", Font.ITALIC, 18));
+
+        window.getContentPane().add(labelNome);
+        window.getContentPane().add(inputNome);
+        // Linha Branca;
+        // window.getContentPane().add(new JLabel());
+        window.getContentPane().add(buttonNome);
+        window.getContentPane().add(outputLabel);
+
+        //configurando o Button;
+        buttonNome.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e ){
+                // função que trata o evento; 
+                buttonClick(e);
+            };
+        });
+
+        // Deixa do tamanho necessario para os itens;
+        // window.pack(); 
+
+        // Adicionando swing objetos;
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    //Fim do main;
+    };
+
+    public static void buttonClick(ActionEvent e ){
+        // Evento que acontece quando chamado o Botão;
+        //JOptionPane.showMessageDialog(null, "Botão Funciona", "Oi", JOptionPane.INFORMATION_MESSAGE);
+
+        String seuNome = inputNome.getText();
+        String outputMensagem = "Olá " + seuNome;
+        outputLabel.setText(outputMensagem);
     }
 }
---
+++ PrimeiroProjetoJava/src/main/java/br/com/estudando/primeiroprojetojava/PrimeiroProjetoJava.java
@@ -0,0 +1,8 @@
+package br.com.estudando.primeiroprojetojava;
+
+public class PrimeiroProjetoJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
